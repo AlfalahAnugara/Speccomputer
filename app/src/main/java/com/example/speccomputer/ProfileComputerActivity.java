0a52583af9eca467229defff1ae0a85e2692bd88
AlfalahAnugara <Alfalah190299@gmail.com>
2019-10-12 13:28:25 +0700

    ProfileComputerActivity Done

--- app/src/main/java/com/example/speccomputer/ProfileComputerActivity.java
+++ app/src/main/java/com/example/speccomputer/ProfileComputerActivity.java
@@ -3,12 +3,35 @@ package com.example.speccomputer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class ProfileComputerActivity extends AppCompatActivity {
+
+    private EditText ramInput ;
+    private EditText procInput ;
+    private EditText vgaInput ;
+    private EditText moboInput ;
+    private EditText psuInput ;
+    private EditText casingInput ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_computer);
+        ramInput = findViewById(R.id.input_ram) ;
+        procInput = findViewById(R.id.input_proc) ;
+        vgaInput = findViewById(R.id.input_vga) ;
+        moboInput = findViewById(R.id.input_mobo) ;
+        psuInput = findViewById(R.id.input_psu) ;
+        casingInput = findViewById(R.id.input_casing) ;
+
+        Bundle extras = getIntent().getExtras() ;
+
+        if (extras != null) {
+
+            User u = extras.getParcelable("user") ;
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/speccomputer/User.java
@@ -0,0 +1,4 @@
+package com.example.speccomputer;
+
+public class User {
+}
