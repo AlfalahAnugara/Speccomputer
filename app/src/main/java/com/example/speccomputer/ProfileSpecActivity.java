c23192d695ed6d676b146b4c80c15cc81655ab07
AlfalahAnugara <Alfalah190299@gmail.com>
2019-10-12 13:52:51 +0700

    menambah ProfileSpecActivity dan activity_profile_spec

---
+++ app/src/main/java/com/example/speccomputer/ProfileSpecActivity.java
@@ -0,0 +1,14 @@
+package com.example.speccomputer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileSpecActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_spec);
+    }
+}
